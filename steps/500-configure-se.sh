267a307ca67bde84247101772af5082a8ad6f770
Lukas Zapletal <lzap+git@redhat.com>
2013-02-20 10:07:08 +0100

    support for admin option

--- steps/500-configure-se.sh
+++ steps/500-configure-se.sh
@@ -1,10 +1,13 @@
 echo "katello" > /root/my-oauth-token-file
 
-# se 1.0 does not support many configuration options
-if [ "$(rpm -q --queryformat '%{VERSION}' katello)" == "0.1.311" ]; then
+KATELLO_VER=$(rpm -q --queryformat '%{VERSION}' katello)
+
+# various se versions support different parameters
+if [[ $KATELLO_VER =~ ^0.1.311 ]]; then
   katello-configure --deployment=cfse
-else
-  # for all other versions use generic configuration
+elif [[ $KATELLO_VER =~ ^1.1.12 ]]; then
   katello-configure --deployment=cfse $KATELLO_CONFIGURE_OPTIONS --oauth-token-file=/root/my-oauth-token-file
+else
+  katello-configure --deployment=cfse $KATELLO_CONFIGURE_OPTIONS --oauth-token-file=/root/my-oauth-token-file --user-pass=admin
 fi
 
