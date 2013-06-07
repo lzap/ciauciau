64397b79eed858d156649af912876e9646c9d5ba
Lukas Zapletal <lzap+git@redhat.com>
2013-06-07 17:25:07 +0200

    adding foreman plan

---
+++ steps/400-install-foreman.sh
@@ -0,0 +1,2 @@
+yum -y install http://yum.theforeman.org/rc/el6/x86_64/foreman-release-1.2.0-0.3.RC2.el6.noarch.rpm
+yum -y install foreman-installer
---
+++ steps/500-configure-foreman.sh
@@ -0,0 +1,1 @@
+echo foreman_installer | puppet apply --modulepath /usr/share/foreman-installer
