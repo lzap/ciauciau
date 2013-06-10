bc0af5a58fe00b7d22e898f2460ff78c4841e2ee
Lukas Zapletal <lzap+git@redhat.com>
2013-06-10 20:39:32 +0200

    nightly

--- steps/500-configure-foreman-nightly.sh
+++ steps/500-configure-foreman-nightly.sh
@@ -1,15 +1,22 @@
 # this file is installed by foreman-release rpm
 rm -f /etc/yum.repos.d/foreman.repo
 
+# there is a bug in puppet - must configure this manually
+if [ -f /etc/fedora-release ]; then
+cat > /etc/yum.repos.d/foreman-nightly.repo <<EOS
+[foreman-nightly]
+baseurl=$MIRROR/releases/yum/foreman-nightly/Fedora/\$releasever/x86_64/
+gpgcheck=0
+EOS
+else
+cat > /etc/yum.repos.d/foreman-nightly.repo <<EOS
+[foreman-nightly]
+baseurl=$MIRROR/releases/yum/foreman-nightly/RHEL/6/x86_64/
+gpgcheck=0
+EOS
+fi
+
 puppet apply --modulepath /usr/share/foreman-installer --verbose <<EOS
-\$osmajor = regsubst(\$::operatingsystemrelease, '\..*', '')
-yumrepo { "foreman-nightly":
-  baseurl => \$::operatingsystem ? {
-    fedora  => "$MIRROR/releases/yum/foreman-nightly/Fedora/\${::operatingsystemrelease}/x86_64/",
-    default => "$MIRROR/releases/yum/foreman-nightly/RHEL/\${osmajor}/x86_64/",
-  },
-  gpgcheck => 0,
-} ->
 class {'puppet':
 } ->
 class {'puppet::server':
