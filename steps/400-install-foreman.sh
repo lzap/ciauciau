21d27ad37faaab97f923991ba63333d7a500d294
Lukas Zapletal <lzap+git@redhat.com>
2013-07-10 13:43:25 +0200

    foreman stable 1.2 now

--- steps/400-install-foreman.sh
+++ steps/400-install-foreman.sh
@@ -1,2 +1,2 @@
-yum -y install http://yum.theforeman.org/rc/el6/x86_64/foreman-release-1.2.0-0.3.RC2.el6.noarch.rpm
+yum -y install http://yum.theforeman.org/releases/1.2/el6/x86_64/foreman-release.rpm
 yum -y install foreman-installer foreman-selinux
